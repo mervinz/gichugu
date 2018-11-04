bd5251a57017d027d1f791da6df11d5d16586c04
marvinz <benedictephantus@gmail.com>
2018-11-04 13:53:21 +0300

    scaffolding for tailwind css added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +12,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+
+mix.postCss('resources/css/main.css', 'public/css', [
+  tailwindcss('tailwind.js'),
+]);
